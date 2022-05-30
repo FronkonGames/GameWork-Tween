5f6f90be446243d3a1c33e0ea7d713d275bafdb4
Martin Bustos <fronkongames@gmail.com>
2022-05-31 00:18:08 +0200

    Create tweens in Module.

--- Runtime/TweenModule.cs
+++ Runtime/TweenModule.cs
@@ -15,8 +15,8 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 using System;
-// using UnityEngine;
-// using UnityEngine.Pool; TODO
+using UnityEngine;
+// @TODO: Add UnityEngine.Pool; 
 using FronkonGames.GameWork.Foundation;
 using FronkonGames.GameWork.Core;
 
@@ -63,6 +63,146 @@ namespace FronkonGames.GameWork.Modules.TweenModule
                              Func<ITween<float>, bool> conditionFunc = null)
     {
       TweenFloat tween = new TweenFloat();
+      tween.Start(start, end, duration, easing, progressCallback, execution, endCallback, conditionFunc);
+
+      Add(tween);
+
+      return tween;
+    }
+
+    /// <summary>
+    /// Creates a Tween Color.
+    /// </summary>
+    /// <param name="start">Initial value.</param>
+    /// <param name="end">Final value.</param>
+    /// <param name="duration">Duration in seconds of the operation.</param>
+    /// <param name="easing">Easing type.</param>
+    /// <param name="progressCallback">Progress callback. Necessary to update the value.</param>
+    /// <param name="execution">Execution mode: Once, Loop, Yoyo.</param>
+    /// <param name="endCallback">End callback.</param>
+    /// <param name="conditionFunc">Condition function. If the condition is not true, the tween ends.</param>
+    public TweenColor Create(Color start,
+                             Color end,
+                             float duration,
+                             Easing easing,
+                             Action<ITween<Color>> progressCallback,
+                             TweenExecution execution = TweenExecution.Once,
+                             Action<ITween<Color>> endCallback = null,
+                             Func<ITween<Color>, bool> conditionFunc = null)
+    {
+      TweenColor tween = new TweenColor();
+      tween.Start(start, end, duration, easing, progressCallback, execution, endCallback, conditionFunc);
+
+      Add(tween);
+
+      return tween;
+    }
+
+    /// <summary>
+    /// Creates a Tween Vector2.
+    /// </summary>
+    /// <param name="start">Initial value.</param>
+    /// <param name="end">Final value.</param>
+    /// <param name="duration">Duration in seconds of the operation.</param>
+    /// <param name="easing">Easing type.</param>
+    /// <param name="progressCallback">Progress callback. Necessary to update the value.</param>
+    /// <param name="execution">Execution mode: Once, Loop, Yoyo.</param>
+    /// <param name="endCallback">End callback.</param>
+    /// <param name="conditionFunc">Condition function. If the condition is not true, the tween ends.</param>
+    public TweenVector2 Create(Vector2 start,
+                               Vector2 end,
+                               float duration,
+                               Easing easing,
+                               Action<ITween<Vector2>> progressCallback,
+                               TweenExecution execution = TweenExecution.Once,
+                               Action<ITween<Vector2>> endCallback = null,
+                               Func<ITween<Vector2>, bool> conditionFunc = null)
+    {
+      TweenVector2 tween = new TweenVector2();
+      tween.Start(start, end, duration, easing, progressCallback, execution, endCallback, conditionFunc);
+
+      Add(tween);
+
+      return tween;
+    }
+
+    /// <summary>
+    /// Creates a Tween Vector3.
+    /// </summary>
+    /// <param name="start">Initial value.</param>
+    /// <param name="end">Final value.</param>
+    /// <param name="duration">Duration in seconds of the operation.</param>
+    /// <param name="easing">Easing type.</param>
+    /// <param name="progressCallback">Progress callback. Necessary to update the value.</param>
+    /// <param name="execution">Execution mode: Once, Loop, Yoyo.</param>
+    /// <param name="endCallback">End callback.</param>
+    /// <param name="conditionFunc">Condition function. If the condition is not true, the tween ends.</param>
+    public TweenVector3 Create(Vector3 start,
+                               Vector3 end,
+                               float duration,
+                               Easing easing,
+                               Action<ITween<Vector3>> progressCallback,
+                               TweenExecution execution = TweenExecution.Once,
+                               Action<ITween<Vector3>> endCallback = null,
+                               Func<ITween<Vector3>, bool> conditionFunc = null)
+    {
+      TweenVector3 tween = new TweenVector3();
+      tween.Start(start, end, duration, easing, progressCallback, execution, endCallback, conditionFunc);
+
+      Add(tween);
+
+      return tween;
+    }
+
+    /// <summary>
+    /// Creates a Tween Vector4.
+    /// </summary>
+    /// <param name="start">Initial value.</param>
+    /// <param name="end">Final value.</param>
+    /// <param name="duration">Duration in seconds of the operation.</param>
+    /// <param name="easing">Easing type.</param>
+    /// <param name="progressCallback">Progress callback. Necessary to update the value.</param>
+    /// <param name="execution">Execution mode: Once, Loop, Yoyo.</param>
+    /// <param name="endCallback">End callback.</param>
+    /// <param name="conditionFunc">Condition function. If the condition is not true, the tween ends.</param>
+    public TweenVector4 Create(Vector4 start,
+                               Vector4 end,
+                               float duration,
+                               Easing easing,
+                               Action<ITween<Vector4>> progressCallback,
+                               TweenExecution execution = TweenExecution.Once,
+                               Action<ITween<Vector4>> endCallback = null,
+                               Func<ITween<Vector4>, bool> conditionFunc = null)
+    {
+      TweenVector4 tween = new TweenVector4();
+      tween.Start(start, end, duration, easing, progressCallback, execution, endCallback, conditionFunc);
+
+      Add(tween);
+
+      return tween;
+    }
+
+    /// <summary>
+    /// Creates a Tween Quaternion.
+    /// </summary>
+    /// <param name="start">Initial value.</param>
+    /// <param name="end">Final value.</param>
+    /// <param name="duration">Duration in seconds of the operation.</param>
+    /// <param name="easing">Easing type.</param>
+    /// <param name="progressCallback">Progress callback. Necessary to update the value.</param>
+    /// <param name="execution">Execution mode: Once, Loop, Yoyo.</param>
+    /// <param name="endCallback">End callback.</param>
+    /// <param name="conditionFunc">Condition function. If the condition is not true, the tween ends.</param>
+    public TweenQuaternion Create(Quaternion start,
+                                  Quaternion end,
+                                  float duration,
+                                  Easing easing,
+                                  Action<ITween<Quaternion>> progressCallback,
+                                  TweenExecution execution = TweenExecution.Once,
+                                  Action<ITween<Quaternion>> endCallback = null,
+                                  Func<ITween<Quaternion>, bool> conditionFunc = null)
+    {
+      TweenQuaternion tween = new TweenQuaternion();
       tween.Start(start, end, duration, easing, progressCallback, execution, endCallback, conditionFunc);
 
       Add(tween);
